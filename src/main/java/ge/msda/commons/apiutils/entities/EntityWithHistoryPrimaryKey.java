78174539acb386622b16b1998fde84d7569c7f93
Vamekh <vamexinar@gmail.com>
2016-03-18 17:22:00 +0400

    pachage changed

--- src/main/java/ge/msda/commons/apiutils/entities/EntityWithHistoryPrimaryKey.java
+++
@@ -1,9 +0,0 @@
-package ge.msda.commons.apiutils.entities;
-
-public interface EntityWithHistoryPrimaryKey {
-    Long getRecordId();
-    void setRecordId(Long recordId);
-
-    Long getId();
-    void setId(Long id);
-}
