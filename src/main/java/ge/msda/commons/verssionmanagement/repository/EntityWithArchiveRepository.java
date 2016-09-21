d29ddb5fd8cc8ec3d68afd7b86ce2d0a32e03233
Vamekh <vamexinar@gmail.com>
2016-09-21 13:29:00 +0400

    history deleted. action performer added

--- src/main/java/ge/msda/commons/verssionmanagement/repository/EntityWithArchiveRepository.java
+++ src/main/java/ge/msda/commons/verssionmanagement/repository/EntityWithArchiveRepository.java
@@ -15,15 +15,19 @@ import java.util.List;
 @NoRepositoryBean
 public interface EntityWithArchiveRepository<T extends EntityWithArchive, ID extends Serializable> extends JpaRepository<T, ID> {
 
-    @Query(value = "FROM #{#entityName} e WHERE e.id = :itemId AND e.fromDate <= :actionDate AND e.toDate > :actionDate")
+    String E_ACTION_DATE = " e.fromDate <= :actionDate AND e.toDate > :actionDate";
+    String AND_E_ACTION_DATE = " AND" + E_ACTION_DATE;
+
+
+    @Query(value = "FROM #{#entityName} e WHERE e.id = :itemId" + AND_E_ACTION_DATE)
     T findCurrentVersion(@Param("itemId") ID id, @Param("actionDate") Date actionDate);
 
-    @Query(value = "FROM #{#entityName} e WHERE e.fromDate <= :actionDate AND e.toDate > :actionDate")
+    @Query(value = "FROM #{#entityName} e WHERE " + E_ACTION_DATE)
     Page<T> findAll(@Param("actionDate") Date actionDate, Pageable pageable);
 
-    @Query(value = "FROM #{#entityName} e WHERE (:itemId IS NULL OR e.id = :itemId )AND e.fromDate <= :actionDate AND e.toDate > :actionDate")
+    @Query(value = "FROM #{#entityName} e WHERE (:itemId IS NULL OR e.id = :itemId ) " + AND_E_ACTION_DATE)
     List<T> search(@Param("itemId") Long itemId, @Param("actionDate") Date actionDate);
 
-    @Query(value = "FROM #{#entityName} e WHERE (:itemId IS NULL OR e.id = :itemId )AND e.fromDate <= :actionDate AND e.toDate > :actionDate")
+    @Query(value = "FROM #{#entityName} e WHERE (:itemId IS NULL OR e.id = :itemId ) " + AND_E_ACTION_DATE)
     Page<T> search(@Param("itemId") Long itemId, @Param("actionDate") Date actionDate, Pageable pageable);
 }
